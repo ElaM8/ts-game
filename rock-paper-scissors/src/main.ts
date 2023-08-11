3bc59f65a7380ecd445102b9906a3c0941635f04
ElaM8 <ela.goodpearl@gmail.com>
2023-08-11 14:31:48 +0100

    commit before deploy

--- rock-paper-scissors/src/main.ts
+++ rock-paper-scissors/src/main.ts
@@ -4,17 +4,17 @@ import Game from './data/types'
 const game: Game = {
   tokenChoices: ["rock", "paper", "scissors", "lizard", "spock"],
   winningTokens: {
-    "rock": ["scissors", "lizard"],
-    "paper": ["rock", "spock"],
-    "scissors": ["paper", "lizard"],
-    "lizard": ["paper", "spock"],
-    "spock": ["rock", "scissors"]
+    rock: ["scissors", "lizard"],
+    paper : ["rock", "spock"],
+    scissors : ["paper", "lizard"],
+    lizard : ["paper", "spock"],
+    spock : ["rock", "scissors"]
   },
   result: {
     draw: 'Its a draw',
     AIWins: 'The computer has beaten the human',
     PlayerWins: 'You have beaten the computer'
-  }  
+  }
 }
 
 const instruction = document.querySelector<HTMLHeadingElement>("h3");
@@ -25,7 +25,7 @@ const displayHumanScore = document.querySelector<HTMLDivElement>(".human-score")
 const displayComputerScore = document.querySelector<HTMLDivElement>(".computer-score");
 
 let AIChoice: string;
-let result: string;
+let winner: string;
 let humanScore: number = 0;
 let computerScore: number = 0;
 displayHumanScore.innerText = humanScore;
@@ -44,62 +44,65 @@ if (!displayChoice ||
 }
 
 const getAIChoice = () => {
-  const math: number = Math.floor(Math.random() * game.tokenChoices.length)
-  AIChoice = game.tokenChoices[math]
+  const { tokenChoices } = game;
+  const math: number = Math.floor(Math.random() * tokenChoices.length);
+  AIChoice = tokenChoices[math];
   return AIChoice
 }
 
   const handleButtonClick = (e: Event) => {
     getAIChoice()
     let userInput = (e.target as HTMLInputElement).innerText.toLowerCase();
-    if (userInput) {
     userInputButtons.forEach(b => b.disabled = true);
     instruction.style.visibility = "hidden"; 
-    displayChoice.innerText = `You have chosen ${userInput}. 
-                              The computer player has chosen ${AIChoice}.`
+    displayChoice.innerText = ` You have chosen ${userInput}. 
+                                The computer player has chosen ${AIChoice}.
+                              `
     return decideWinner(userInput)
-  }
 }
 userInputButtons.forEach(b => b.addEventListener("click", handleButtonClick));
 
 const decideWinner = (userInput: string) => {
-  const { winningTokens } = game
+  const { tokenChoices, winningTokens, result: { draw, PlayerWins, AIWins } } = game
   if (AIChoice === userInput) {
-    result = game.result.draw
-  } else if (userInput === "paper" && (winningTokens["paper"].includes(AIChoice))) {
-    result = game.result.PlayerWins
-  } else if (userInput === "rock" && (winningTokens["rock"].includes(AIChoice))) {
-    result = game.result.PlayerWins
-  } else if (userInput === "scissors" && (winningTokens["scissors"].includes(AIChoice))) {
-    result = game.result.PlayerWins
-  } else if (userInput === "lizard" && (winningTokens["lizard"].includes(AIChoice))) {
-    result = game.result.PlayerWins
-  } else if (userInput === "spock" && (winningTokens["spock"].includes(AIChoice))) {
-    result = game.result.PlayerWins
+    winner = draw
+  } else if (userInput === tokenChoices[0] && AIChoice === "scissors") {
+    winner = "As it always has, rock crushes scissors"
+  } else if (userInput === tokenChoices[0] && (winningTokens[userInput].includes(AIChoice))) {
+    winner = PlayerWins
+  } else if (userInput === tokenChoices[1] && (winningTokens[userInput].includes(AIChoice))) {
+    winner = PlayerWins
+  } else if (userInput === tokenChoices[2] && (winningTokens[userInput].includes(AIChoice))) {
+    winner = PlayerWins
+  } else if (userInput === tokenChoices[3] && (winningTokens[userInput].includes(AIChoice))) {
+    winner = PlayerWins
+  } else if (userInput === tokenChoices[4] && (winningTokens[userInput].includes(AIChoice))) {
+    winner = PlayerWins
   } else {
-    result = game.result.AIWins
+    winner = AIWins
   }
-    return setTimeout(announceWinner, 3000, result)
+    return setTimeout(announceWinner, 3000, winner)
   }
 
-    const announceWinner = (result: string) => {
-    setTimeout(displayResult.innerText = `${result}!!`, 2000)
+    const announceWinner = (winner: string) => {
+    setTimeout(displayResult.innerText = `${winner}!!`, 2000)
     setTimeout(resetGame, 2000)
   }
 
-  // TODO: add button option to reset
-  // TODO: add confetti
-
   const incrementScore = () => {
-      if (result === game.result.draw) {
+      const { draw, PlayerWins, AIWins } = game.result
+    if (winner === draw) {
       computerScore ++;
       humanScore ++;
       displayHumanScore.innerText = humanScore;
       displayComputerScore.innerText = computerScore;
-    } else if (result === game.result.PlayerWins) {
+    } else if (winner === "As it always has, rock crushes scissors"){
+      humanScore += 100;
+      displayHumanScore.innerText = humanScore;
+    } else if (winner === PlayerWins) {
       humanScore ++;
       displayHumanScore.innerText = humanScore;
-    } else if (result === game.result.AIWins) {
+    } else if (winner === AIWins) {
       computerScore ++;
       displayComputerScore.innerText = computerScore;
     } else {
@@ -107,13 +110,10 @@ const decideWinner = (userInput: string) => {
     }
   }
 
-  // TODO: add input for how many games to play. Can just add up both scores and if => input, then were done. 
- // TODO: Also add, we're done final winner announcement 
-
   const resetGame = () => {
     incrementScore();
     AIChoice = "";
-    result = "";
+    winner = "";
     instruction.style.visibility = "visible"; 
     displayChoice.innerText = "";
     displayResult.innerText = "";
